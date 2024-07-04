e286a26c72c2ef1edf2e418a279be4151466df90
Uli Köhler <ukoehler@techoverflow.net>
2024-07-04 02:58:23 +0200

    More fixes

--- scripts/Connector/Connector_JST/conn_jst_jq_tht_top.py
+++ scripts/Connector/Connector_JST/conn_jst_jq_tht_top.py
@@ -42,11 +42,10 @@ pad_copper_y_solder_length = 0.5 #How much copper should be in y direction?
 min_annular_ring = 0.15
 
 #FP name strings
-part_base = "{n}JQ-BT" #JST part number format string
+part_base = "{n:02d}JQ-BT"  # JST part number format string
 
 variant_params = {
     None:{
-        'boss': False,
         'back_protrusion': False,
         'pin_range': chain(range(3,14), [15]) # 14 not available according to datasheet
     },
@@ -62,21 +61,20 @@ pin1_marker_offset = 0.3
 
 def generate_one_footprint(pins, variant, configuration):
     #calculate fp dimensions
-    boss = variant_params[variant]['boss']
     A = (pins - 1) * pitch
     B = A + 4.9
 
     #connector thickness
-    T = 5.75
+    T = 6
 
     #corners
     x1 = -2.45
     x2 = x1 + B
 
-
+ 
     x_mid = (x1 + x2) / 2
 
-    y1 = -2.35
+    y1 = -3.3 # Y coordinate of the pin row, relative to the F.Fab corner (6.0 - smaller datasheet value)
     y2 = y1 + T
 
     #generate the name
@@ -88,14 +86,12 @@ def generate_one_footprint(pins, variant, configuration):
         pitch=pitch, orientation=orientation_str)
 
     kicad_mod = Footprint(footprint_name, FootprintType.THT)
-    kicad_mod.setDescription("JST {:s} series connector, {:s}{:s} ({:s}), generated with kicad-footprint-generator".format(
-        series, mpn, ', with boss' if boss else '', datasheet))
+    kicad_mod.setDescription("JST {:s} series connector, {:s} ({:s}), generated with kicad-footprint-generator".format(
+        series, mpn, datasheet))
 
     tags = configuration['keyword_fp_string'].format(series=series,
         orientation=orientation_str, man=manufacturer,
         entry=configuration['entry_direction'][orientation])
-    if boss:
-        tags += " boss"
     #set the FP tags
     kicad_mod.setTags(tags)
 
@@ -105,10 +101,7 @@ def generate_one_footprint(pins, variant, configuration):
     # set general values
     #kicad_mod.append(Property(name=Property.REFERENCE, text='REF**', at=[x_mid,-3.5], layer='F.SilkS'))
 
-    if pins == 2:
-        drill = 1.0
-    else:
-        drill = 0.95
+    drill = 0.95 # Datasheet: 0.9 +0.1/-0 => center tolerance 0.95 +/- 0.05
 
     pad_size = [pitch - pad_to_pad_clearance, drill + 2*pad_copper_y_solder_length]
     if pad_size[0] - drill < 2*min_annular_ring:
@@ -129,63 +122,9 @@ def generate_one_footprint(pins, variant, configuration):
         type=Pad.TYPE_THT, shape=Pad.SHAPE_OVAL, layers=Pad.LAYERS_THT,
         **optional_pad_params))
 
-    if boss:
-        if pins == 1:
-            boss_y = 4.35
-            boss_x = 0
-            boss_drill = 1.75
-            kicad_mod.append(Pad(type=Pad.TYPE_NPTH, shape=Pad.SHAPE_CIRCLE,
-                                 at=[boss_x, boss_y], size=boss_drill,
-                                 drill=boss_drill, layers=Pad.LAYERS_NPTH))
-        else:
-            boss_y = 2
-            boss_x = -1.6
-            boss_drill = 1.2
-            kicad_mod.append(Pad(type=Pad.TYPE_NPTH, shape=Pad.SHAPE_CIRCLE,
-                                 at=[boss_x, boss_y], size=boss_drill,
-                                 drill=boss_drill, layers=Pad.LAYERS_NPTH))
-
-    if boss and pins == 1:
-        boss_size_1pin = 1.5
-        off = configuration['silk_fab_offset']
-        x_boss_fab_off = boss_size_1pin/2 + off
-        x_boss_pad_off = boss_drill/2 + configuration['silk_line_width']/2 + configuration['silk_pad_clearance']
-        x_boss = x_boss_fab_off
-        if x_boss_pad_off > x_boss:
-            x_boss=x_boss_pad_off
-        out_silk = PolygonLine(polygon=[
-            {'x': -x_boss, 'y':boss_y},
-            {'x': -x_boss, 'y':y2+off},
-            {'x': x1-off, 'y':y2+off},
-            {'x': x1-off, 'y':y1-off},
-            {'x': x2+off, 'y':y1-off},
-            {'x': x2+off, 'y':y2+off},
-            {'x': x_boss, 'y':y2+off},
-            {'x': x_boss, 'y':boss_y}
-        ], layer='F.SilkS', width=configuration['silk_line_width'])
-        kicad_mod.append(out_silk)
-        kicad_mod.append(Arc(center=[0,boss_y], start=[-x_boss, boss_y], angle=-180,
-            layer="F.SilkS", width=configuration['silk_line_width']))
-
-        out_fab = PolygonLine(polygon=[
-            {'x': -boss_size_1pin/2, 'y':boss_y},
-            {'x': -boss_size_1pin/2, 'y':y2},
-            {'x': x1, 'y':y2},
-            {'x': x1, 'y':y1},
-            {'x': x2, 'y':y1},
-            {'x': x2, 'y':y2},
-            {'x': boss_size_1pin/2, 'y':y2},
-            {'x': boss_size_1pin/2, 'y':boss_y}
-        ], layer='F.Fab', width=configuration['fab_line_width'])
-        kicad_mod.append(out_fab)
-        kicad_mod.append(Arc(center=[0,boss_y], start=[-boss_size_1pin/2, boss_y], angle=-180,
-            layer="F.Fab", width=configuration['fab_line_width']))
-
-
-    else:
-        out = RectLine(start=[x1,y1], end=[x2,y2], offset=configuration['silk_fab_offset'],
-            layer='F.SilkS', width=configuration['silk_line_width'])
-        kicad_mod.append(out)
+    out = RectLine(start=[x1,y1], end=[x2,y2], offset=configuration['silk_fab_offset'],
+        layer='F.SilkS', width=configuration['silk_line_width'])
+    kicad_mod.append(out)
     body_edge={'left':x1, 'right':x2, 'top':y1, 'bottom':y2}
 
     #draw the courtyard
@@ -194,8 +133,6 @@ def generate_one_footprint(pins, variant, configuration):
 
     cx2 = roundToBase(x2+configuration['courtyard_offset']['connector'], configuration['courtyard_grid'])
     cy2 = roundToBase(y2+configuration['courtyard_offset']['connector'], configuration['courtyard_grid'])
-    if boss and pins == 1:
-        cy2 = roundToBase(boss_y+boss_drill/2+configuration['courtyard_offset']['connector'], configuration['courtyard_grid'])
 
     kicad_mod.append(RectLine(
         start=[cx1, cy1], end=[cx2, cy2],
@@ -215,10 +152,10 @@ def generate_one_footprint(pins, variant, configuration):
         kicad_mod.append(PolygonLine(polygon=poly_fab_marker, layer='F.Fab', width=configuration['fab_line_width']))
 
     #wall thickness w
-    w = 0.75
+    wall_thickness = 1.2 # measured from 3d model
 
     #gap size g
-    g = 1.5
+    g = 1.0 # measured from 3d model
 
     off = 0.1
 
@@ -227,33 +164,28 @@ def generate_one_footprint(pins, variant, configuration):
 
     x2 += off
     y2 += off
+    
+    # NOTE: Adjusted so that gap start matches wall thickness for JQ connector
+    gap_x_offset = -0.7 # Relative to the X coord of the first/last pin. Inverted for last pin.
 
     #draw the center tab
-    kicad_mod.append(RectLine(start=[g/2,y1], end=[A-g/2,y1+w], layer='F.SilkS', width=configuration['silk_line_width']))
+    kicad_mod.append(RectLine(start=[g/2+gap_x_offset,y1], end=[A-g/2-gap_x_offset,y1+wall_thickness], layer='F.SilkS', width=configuration['silk_line_width']))
 
-    #add left tab
-    kicad_mod.append(RectLine(start=[x1,y1], end=[-g/2,y1+w], layer='F.SilkS', width=configuration['silk_line_width']))
-    #right tab
-    kicad_mod.append(RectLine(start=[A+g/2,y1], end=[x2,y1+w], layer='F.SilkS', width=configuration['silk_line_width']))
-
-    #add other line
+    #add vertical lines signifying the part of the plastic contacting the PCB
     line = [
-    {'x': x1,'y': y1+w+g},
-    {'x': x1+w,'y': y1+w+g},
-    {'x': x1+w,'y': y2-w},
-    {'x': A/2,'y': y2-w}
+        {'x': x1+wall_thickness,'y': y1},
+        {'x': x1+wall_thickness,'y': y2},
     ]
-    if boss and pins > 1:
-        line2 = line[:2]
-        line2.append({'x': x1+w,'y': boss_y - boss_drill/2 - configuration['silk_line_width']/2 - configuration['silk_pad_clearance']})
-        kicad_mod.append(PolygonLine(polygon=line2, layer='F.SilkS', width=configuration['silk_line_width']))
-
-        kicad_mod.append(Line(start=[A/2, y2-w],
-            end=[boss_x+boss_drill/2+ configuration['silk_line_width']/2 + configuration['silk_pad_clearance'], y2-w],
-            layer='F.SilkS', width=configuration['silk_line_width']))
-    else:
-        kicad_mod.append(PolygonLine(polygon=line, layer='F.SilkS', width=configuration['silk_line_width']))
+    kicad_mod.append(PolygonLine(polygon=line, layer='F.SilkS', width=configuration['silk_line_width']))
     kicad_mod.append(PolygonLine(polygon=line, x_mirror=A / 2, layer='F.SilkS', width=configuration['silk_line_width']))
+
+    # plastic part cutout on the south side
+    south_side_gap_size = 1.0 # mm, measured from 3d model
+    kicad_mod.append(PolygonLine(polygon=[
+        # Between the two vertical lines
+        {'x': x1+wall_thickness, 'y': y2-south_side_gap_size},
+        {'x': x2-wall_thickness, 'y': y2-south_side_gap_size},
+    ], layer='F.SilkS', width=configuration['silk_line_width']))
 
     #pin-1 marker
     y =  -2.75
